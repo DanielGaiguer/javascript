f3724114c2b1c895d00ffd57c6a0648e10c4798c
DanielGaiguer <gaiguer2019@gmail.com>
2024-09-30 22:12:28 -0300

    aula 11

---
+++ aulas/aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log (` A velcidade de seu carro é de ${vel} km/h`)
+if (vel > 60 ){
+    console.log ('Voce esta acima da velocidade ideal, multado!')
+}//condição simples
+console.log ('Dirija sempre usando cinto de segurança')

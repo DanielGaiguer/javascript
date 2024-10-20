836789719da71f121e87282059605e3593d10250
DanielGaiguer <gaiguer2019@gmail.com>
2024-10-20 00:26:22 -0300

    objeto01

---
+++ aulas/aula017/objeto01.js
@@ -0,0 +1,9 @@
+let amigo = {nome: `jose`, 
+    sexo: `M`, 
+    peso: '85.5', 
+    engordar(p = 0 ){
+        console.log ('engordou')
+    }}
+    let num = 8
+console.log (`${amigo.nome} pesa ${amigo.peso}`)
+console.log (num **= 2)

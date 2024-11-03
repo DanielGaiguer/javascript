6ea3a40d3e3ffb093f6086cb1afa30bc39d2da6a
DanielGaiguer <gaiguer2019@gmail.com>
2024-11-03 16:08:36 -0300

    ex26

--- aulas udemy/aula026ex/assets/js/main.js
+++ aulas udemy/aula026ex/assets/js/main.js
@@ -19,11 +19,42 @@ form.addEventListener('submit', function(e) {
         setResultado('Altura Invalida!', false)
         return
     }
-    
     const imc = getImc(peso, altura)
-    console.log (imc)
+    const nivel = getNivelImc(imc)
+    const msg = `Seu IMC é ${imc} (${nivel})`
+    setResultado(msg, true)
 } )
 
+function getNivelImc(imc){
+    let nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau', 'Obesidade grau II ou severa', 'Obesidade grau III ou mórbida']
+    /*switch (imc){
+        case (imc < 18.5) : 
+            grau = nivel[0]
+        break
+        case (imc < 24.9) : 
+            grau = nivel[1]
+        break
+        case (imc < 29.9):
+            grau = nivel[2]
+        break
+        case (imc < 34.9):
+            grau = nivel[3]
+        break
+        case (imc < 39.9):
+            grau = nivel[4]
+        break
+        case (imc > 40) :
+            grau = nivel[5]
+        break 
+    }
+    setResultado(grau)*/
+    if (imc < 18.5) return nivel[0]
+    if (imc < 24.9) return nivel[1]
+    if (imc < 29.9) return nivel[2]
+    if (imc < 34.9) return nivel[3]
+    if (imc < 39.9) return nivel[4]
+    if (imc > 40) return nivel[5]
+}
 function criaP(){
     let p = document.createElement('p')
     return p 
@@ -35,6 +66,11 @@ function setResultado(msg, isValid){
     const p = criaP()
     p.innerHTML = msg
     resultado.appendChild(p)
+    if (isValid){
+       p.classList.add('paragrafo-resultado') 
+    } else{
+        p.classList.add('bad') 
+    }
 }
 
 function getImc(peso, altura){

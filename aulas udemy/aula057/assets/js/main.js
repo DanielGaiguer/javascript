a1239778ce2a2a1f3242f083e71077777931568d
DanielGaiguer <gaiguer2019@gmail.com>
2024-11-19 23:54:39 -0300

    aula057

---
+++ aulas udemy/aula057/assets/js/main.js
@@ -0,0 +1,75 @@
+function getCalculator(){
+    return{
+        display: document.querySelector(".display"),
+        
+        inicia() {
+            this.clickbutton()
+            //this.clearDisplay()
+            this.enter()
+        },
+
+        clearDisplay(){
+            this.display.value = ''
+        },
+
+        delOne(){
+            this.display.value = this.display.value.slice(0, -1)
+        },
+
+        equal() {
+            let conta = this.display.value
+
+            try{
+                conta = Number(eval(conta))
+
+                if(!conta) {
+                    alert('conta invalida')
+                    return
+                }
+
+                this.display.value = String(conta)
+            }catch(e){
+                alert ('erro')
+                return
+            }
+        },
+
+        enter(){
+            this.display.addEventListener('keyup', e => {
+                if(e.keyCode === 13 ){
+                    this.equal()
+                    return
+                }
+            })
+        },
+
+        clickbutton(){
+            document.addEventListener('click', (e) =>{
+                const el = e.target
+
+                if (el.classList.contains('btn-num')){
+                    this.btnForDisplay(el.innerText)
+                }
+
+                if (el.classList.contains('btn-clear')){
+                    this.clearDisplay()
+                }
+
+                if (el.classList.contains('btn-del')){
+                    this.delOne()
+                }
+
+                if(el.classList.contains('btn-eq')){
+                    this.equal()
+                }
+            })
+        },
+
+        btnForDisplay(valor){
+            this.display.value += valor
+        }
+    }
+}
+
+const calculator = getCalculator()
+calculator.inicia()

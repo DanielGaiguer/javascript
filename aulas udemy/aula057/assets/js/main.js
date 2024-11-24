0878f6a1abdf684aa543fcee54709a52cb8ae607
DanielGaiguer <gaiguer2019@gmail.com>
2024-11-24 15:39:22 -0300

    Update main.js

--- aulas udemy/aula057/assets/js/main.js
+++ aulas udemy/aula057/assets/js/main.js
@@ -1,4 +1,4 @@
-function getCalculator(){
+/*function getCalculator(){
     return{
         display: document.querySelector(".display"),
         
@@ -35,7 +35,7 @@ function getCalculator(){
         },
 
         enter(){
-            this.display.addEventListener('keyup', e => {
+            document.addEventListener('keyup', e => {
                 if(e.keyCode === 13 ){
                     this.equal()
                     return
@@ -67,9 +67,85 @@ function getCalculator(){
 
         btnForDisplay(valor){
             this.display.value += valor
+            this.display.focus()
         }
     }
 }
 
 const calculator = getCalculator()
 calculator.inicia()
+*/
+
+function Calculadora() {
+    this.display = document.querySelector(".display")
+
+    this.inicia = () => {
+        this.clickButton()
+        this.enter()
+    }
+
+    this.clearDisplay = () => {
+        this.display.value = ''
+    }
+
+    this.enter = () => {
+            document.addEventListener('keyup', e => {
+            if(e.keyCode === 13 ){
+                this.equal()
+                return
+            }
+        })
+    }
+
+    this.delOne = () => {
+        this.display.value = this.display.value.slice(0, -1)
+    }
+
+    this.equal = () => {
+        let conta = this.display.value
+
+        try{
+            conta = Number(eval(conta))
+
+            if(!conta) {
+                alert('conta invalida')
+                return
+            }
+
+            this.display.value = String(conta)
+        }catch(e){
+            alert ('erro')
+            return
+        }
+    }
+
+    this.clickButton = () => {
+        document.addEventListener('click', (e) =>{
+            const el = e.target
+
+            if (el.classList.contains('btn-num')){
+                this.btnForDisplay(el.innerText)
+            }
+
+            if (el.classList.contains('btn-clear')){
+                this.clearDisplay()
+            }
+
+            if (el.classList.contains('btn-del')){
+                this.delOne()
+            }
+
+            if(el.classList.contains('btn-eq')){
+                this.equal()
+            }
+        })
+    }
+
+    this.btnForDisplay = (valor) => {
+        this.display.value += valor
+        this.display.focus()
+    }
+
+}
+const calculadora = new Calculadora()
+calculadora.inicia()

4096e05e57fb868062d4713f5ca251ca4ddc1856
DanielGaiguer <gaiguer2019@gmail.com>
2025-01-14 23:10:14 -0300

    funcao geradora

---
+++ aulas udemy/aula096.ger-sen/src/main.js
@@ -0,0 +1,3 @@
+import formGeraSenha from './modules/formGeraSenha'
+const senha = formGeraSenha
+import './assets/css/style.css'
---
+++ aulas udemy/aula096.ger-sen/src/modules/formGeraSenha.js
@@ -0,0 +1,7 @@
+//import
+export default class formGeraSenha{
+    constructor(){
+        const form = document.querySelectorAll(`.formulario`)
+        console.log(form)
+    }
+}

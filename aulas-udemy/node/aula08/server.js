ee5aa597a021efb9a34a20e7a1b13cce5f2f36f2
DanielGaiguer <gaiguer2019@gmail.com>
2025-01-21 22:44:08 -0300

    create aula08

---
+++ aulas-udemy/node/aula08/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('<form action="/" method= "POST">Hello, World <button> Enviar </buttom> </form>')
+})
+
+
+app.post('/', (req, res) => {
+    res.send(`recebi o formulario `)
+})
+
+app.listen(3333, () => {
+    console.log(`Acessar http://localhost:3333`)
+    console.log(`servidor executando na porta 3333`)
+})
+
+
+/*
+        CRIAR   LER   ATUALIZAR    APAGAR
+CRUD -> CREATE, READ, UPDATE,     DELETE
+         POST,  GET   PUT         DELETE
+*/

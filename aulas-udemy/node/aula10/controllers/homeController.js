36c2b336b2a6f8653852db054877b955236cf990
DanielGaiguer <gaiguer2019@gmail.com>
2025-01-22 17:16:12 -0300

    aula010

--- aulas-udemy/node/aula10/controllers/homeController.js
+++
@@ -1,7 +0,0 @@
-exports.paginaIncial = (req, res) => {
-    res.send('<form action="/" method= "POST"> Nome do cliente: <input type="text" name="nome"> <button> Enviar </buttom> </form>')
-}
-
-exports.trataPost = (req, res ) => {
-    res.send(`Sou sua nova rota de post`)
-}
--- aulas-udemy/node/aula10/routes.js
+++ aulas-udemy/node/aula10/routes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const route = express.Router()
-const homeControler = require(`./controllers/homeController`)
-const contatoController = require('./controllers/contatoController')
+const homeControler = require(`./src/controllers/homeController`)
+
 
 route.get('/', homeControler.paginaIncial)
 route.post('/', homeControler.trataPost)
 
-route.get('/contato', contatoController.paginaInicial)
+
 
 module.exports = route

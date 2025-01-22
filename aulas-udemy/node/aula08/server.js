f79d6656be2f279743459cffe4db90e1b070bd97
DanielGaiguer <gaiguer2019@gmail.com>
2025-01-21 23:31:30 -0300

    testes aula08

--- aulas-udemy/node/aula08/server.js
+++ aulas-udemy/node/aula08/server.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const app = express()
 
+app.use(express.urlencoded({extended: true}))
+
 app.get('/', (req, res) => {
-    res.send('<form action="/" method= "POST">Hello, World <button> Enviar </buttom> </form>')
+    res.send('<form action="/" method= "POST"> Nome do cliente: <input type="text" name="nome"> <button> Enviar </buttom> </form>')
 })
 
-
+app.get('/testes/:idUsuarios?/:parametro?', (req, res) => {
+    // profiles/1
+    //profiles/?chave1=valor1&chave2=valor2
+    
+    console.log(req.params)
+    console.log(req.query)
+    res.send(req.params.idUsuarios)
+})  
+ 
 app.post('/', (req, res) => {
-    res.send(`recebi o formulario `)
-})
+    console.log(req.body)
+    res.send(`O que voce me enviou foi ${req.body.nome}`)
+}) 
 
 app.listen(3333, () => {
     console.log(`Acessar http://localhost:3333`)

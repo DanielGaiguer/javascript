44d325a511afc4ca2d365c3f6a952dd638d72b61
DanielGaiguer <gaiguer2019@gmail.com>
2025-01-27 19:02:28 -0300

    Update server.js

--- aulas-udemy/node/aula17/server.js
+++ aulas-udemy/node/aula17/server.js
@@ -21,6 +21,7 @@ const {middlewareGlobal, checkCsrfError, csrfMidlleware} = require(`./src/middle
 
 app.use(helmet())
 app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'public')))
 
 const sessionOptions = session({

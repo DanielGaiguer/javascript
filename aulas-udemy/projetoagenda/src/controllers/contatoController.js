c92dfb48b69fb8a46e2f4760c2f5b4d80eb5d587
DanielGaiguer <gaiguer2019@gmail.com>
2025-01-30 16:57:17 -0300

    route navbar

--- aulas-udemy/projetoagenda/src/controllers/contatoController.js
+++ aulas-udemy/projetoagenda/src/controllers/contatoController.js
@@ -1,3 +1,3 @@
 exports.index = (req, res) => {
-    res.render(`contato`)
+    res.render('contato')
 }
--- aulas-udemy/projetoagenda/src/middlewares/middleware.js
+++ aulas-udemy/projetoagenda/src/middlewares/middleware.js
@@ -21,3 +21,13 @@ exports.middlewareGlobal = (req, res, next) => {
     res.locals.csrfToken = req.csrfToken();
     next();
   };
+
+  exports.loginRequired = (req, res, next) => {
+    if (!req.session.user) {
+      req.flash('errors', 'Você precisa fazer login para continuar.')
+      req.session.save(() => res.redirect('/'))
+      return 
+    }
+
+    next()
+  } 
